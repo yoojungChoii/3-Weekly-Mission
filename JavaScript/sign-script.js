77f64e36a858e31c5a78a32716760c6b490dc10f
Yoojungchoii <geniusyj@gmail.com>
2023-12-22 17:22:04 +0900

    signin page error action added, email confirm error fixed

--- JavaScript/sign-script.js
+++ JavaScript/sign-script.js
@@ -1,6 +1,7 @@
-// const emailInput = document.getElementsByClassName('email')[0];
-// const errorMessage = document.getElementsByClassName('error-message')[0];
+
 const emailInput = document.querySelectorAll('.email')[0];
+const TEST_CORRECT_EMAIL = 'test@codeit.com';
+const TEST_CORRENT_PASSWORD = 'codeit101';
 const passInput = document.querySelectorAll('.password')[0];
 const passConfirmInput = document.querySelectorAll('.password-confirm')[0];
 const errorMail = document.querySelectorAll('.error-mail')[0];
@@ -9,36 +10,64 @@ const errorPassConfirm = document.querySelectorAll('.error-passconfirm')[0];
 const eyeImagePass = document.querySelector('#eyeImagePass'); 
 const eyeImageConfirm = document.querySelector('#eyeImageConfirm'); 
 const pattern = /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/;
-let link = '/folder';
+const VALID_LINK = '/folder';
 
-function checkEmail(emailType) {
+function checkEmail() {
+  const emailInputValue = emailInput.value
+  
   if (emailInput.value === '') {
     errorMail.textContent = '이메일을 입력해주세요.';
     emailInput.classList.add('error'); // 에러 클래스 추가
-  } else if (!pattern.test(emailType)) {
+    return;
+  } 
+  
+  if (!pattern.test(emailInputValue)) {
     errorMail.textContent = '올바른 이메일 주소가 아닙니다.';
     emailInput.classList.add('error'); // 에러 클래스 추가
-  } else if (emailInput.value !== '') {
+    return;
+  } 
+
+  if (emailInput.value === TEST_CORRECT_EMAIL) {
+    errorMail.textContent = '이미 사용 중인 이메일입니다.';
+    emailInput.classList.add('error'); // 에러 클래스 추가
+    return;
+  }
+  
+  if (emailInput.value !== '') {
     errorMail.textContent = '이메일을 확인해주세요.';
     emailInput.classList.add('error'); // 에러 클래스 추가
-  } else if (emailInput.value === 'test@codeit.com' && passInput.value === 'codeit101') {
-    location.href = link;
-  } else {
-    errorMail.textContent = '';
-    emailInput.classList.remove('error'); // 에러 클래스 제거
+    return;
+  } 
+  
+  if (emailInput.value === TEST_CORRECT_EMAIL && passInput.value === TEST_CORRENT_PASSWORD ) {
+    location.href = VALID_LINK;
+    return;
   }
+
+  // errorMail.textContent = '';
+  // emailInput.classList.remove('error'); // 에러 클래스 제거
 }
 
 function checkPass() {
+  const passwordValue = passInput.value;
+  const hasMinLength = passwordValue.length >= 8;
+  const hasLetterAndNumber = /[a-zA-Z]/.test(passwordValue) && /\d/.test(passwordValue);
+
   if (passInput.value === '') {
     errorPass.textContent = '비밀번호를 입력해주세요.';
     passInput.classList.add('error'); // 에러 클래스 추가
-  } else if (passInput.value !== '') {
+    return;
+  }
+
+  if (!hasMinLength || !hasLetterAndNumber) {
+    errorPass.textContent = '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.';
+    return;
+  }
+
+  if (passInput.value !== '') {
     errorPass.textContent = '비밀번호를 확인해주세요.';
     passInput.classList.add('error'); // 에러 클래스 추가
-  } else {
-    errorPass.textContent = '';
-    passInput.classList.remove('error'); // 에러 클래스 제거
+    return;
   }
 }
 
