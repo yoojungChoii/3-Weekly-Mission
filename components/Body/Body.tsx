0f265273688df393579d30fa6547316be5df911a
Yoojungchoii <geniusyj@gmail.com>
2024-02-23 18:37:29 +0900

    ✨ 저장된 링크가 없을 때 없다고 표시

--- components/Body/Body.tsx
+++ components/Body/Body.tsx
@@ -1,12 +1,18 @@
 import styles from "./Body.module.css";
 import SearchBar from "./SearchBar";
 import CardList from "../UI/Card/CardList";
+import FetchFolderData from "@/get-data/FetchFolderData";
 
 function Body() {
+  const folderdata = FetchFolderData();
   return (
     <div className={styles.body}>
       <SearchBar />
-      <CardList />
+      {folderdata === null ? (
+        <CardList />
+      ) : (
+        <div className={styles["no-links"]}>저장된 링크가 없습니다.</div>
+      )}
     </div>
   );
 }
